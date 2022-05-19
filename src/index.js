78bb78d2d2555d032d4ebc45b9858ecb75907392
KHITER Med Achraf <khiter.med7@gmail.com>
2022-05-19 15:33:33 +0100

    Add imports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './style.css';
+// import * as variables from './modules/variables.js';
+// import * as fetchApi from './modules/fetchApi.js';
+// import * as displayData from './modules/displayData.js';
