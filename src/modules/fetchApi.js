9cde4e202766b0e80c56d07e629974fc21e04606
KHITER Med Achraf <khiter.med7@gmail.com>
2022-05-21 01:42:36 +0100

    Structure the postData function

--- src/modules/fetchApi.js
+++ src/modules/fetchApi.js
@@ -26,20 +26,25 @@ class FetchAPI {
   }
 
   static async postData(name, score) {
-    const object = {
-      user: name,
-      score,
-    };
-    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/I5zbUnax7uOS1kQCeVNl/scores/', {
-      method: 'POST', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(object),
-    })
-      .then((response) => response.json())
-      .then((data) => data)
-      .catch((error) => error);
+    if (name && score) { // name and score exit then post them and display notification.
+      const object = {
+        user: name,
+        score,
+      };
+      fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/I5zbUnax7uOS1kQCeVNl/scores/', {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(object),
+      })
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((error) => error);
+      // display notif
+    } else { // name and score exit then post them and display notification.
+      // coming later.
+    }
   }
 }
 export default FetchAPI;
